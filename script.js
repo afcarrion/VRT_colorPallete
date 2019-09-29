3860eae41440bbf0b3f7ec78708528b2f42b3984
afcarrion <af.carrion@uniandes.edu.co>
2019-09-29 16:47:56 -0500

    Funcionalidad limpiar paleta

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function randomPalette(){
+const reglas = `.website-background{ color: #FFFFFF;}\n
+                .element-text{ color: #FFFFFF;}\n
+                .element-border{ border-color: #FFFFFF;}\n
+                .element-background{ background-color: #FFFFFF;}\n
+                .header{ color: #FFFFFF;}`;
 
+function limpiarPaleta() {
+  $("#css-rules").val(reglas);
 }
 
+function randomPalette() {}
 
-
-function generateRules(){
-
-}
+function generateRules() {}
