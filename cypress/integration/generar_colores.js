9deb5034b859567a82c4c2ed17139d8bacce7610
afcarrion <af.carrion@uniandes.edu.co>
2019-09-29 19:41:47 -0500

    ScreenShot de cypress

--- cypress/integration/generar_colores.js
+++ cypress/integration/generar_colores.js
@@ -1,6 +1,28 @@
 describe("Generacion de Colores", function() {
   it("Visitar la pagina para generar paleta de colores", function() {
     cy.visit("https://afcarrion.github.io/VRT_colorPallete/palette.html");
-    cy.contains("botonGenerar").click();
+    cy.screenshot("Primero de generacion");
+    cy.get(".buttons")
+      .contains("Generar")
+      .click();
+    cy.wait(2000);
+    cy.screenshot("Segundo de generacion");
+  });
+});
+
+describe("Limpiar Paleta de Colores", function() {
+  it("Visitar la pagina para generar paleta de colores", function() {
+    cy.visit("https://afcarrion.github.io/VRT_colorPallete/palette.html");
+    cy.screenshot("Primero de Limpiar");
+    cy.get(".buttons")
+      .contains("Generar")
+      .click();
+    cy.wait(650);
+    cy.get(".buttons")
+      .contains("Limpiar")
+      .click()
+      .end();
+    cy.wait(2000);
+    cy.screenshot("Segundo de Limpiar");
   });
 });
